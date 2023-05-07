4fac7c1c8c30b255f08f7f92b7c21e29089418d6
CodeMode365 <codemode365@gmail.com>
2023-05-07 20:54:07 +0100

    solved

---
+++ dist/recursion/binarySearch.js
@@ -0,0 +1,14 @@
+"use strict";
+const sortedArr = [17, 21, 25, 28, 29, 30, 43, 54, 65, 67, 74, 75, 83, 98];
+const binarySearch = (arr, start, end, target) => {
+    if (start > end)
+        return -1;
+    const midInd = Math.floor((start + end) / 2);
+    if (target == arr[midInd])
+        return midInd;
+    else if (target > arr[midInd])
+        return binarySearch(arr, midInd + 1, end, target);
+    else
+        return binarySearch(arr, start, midInd - 1, target);
+};
+console.log(binarySearch(sortedArr, 0, sortedArr.length - 1, 54));
