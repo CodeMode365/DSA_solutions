b7c5c931c984bd29f917560b1bb9d1d08778884e
CodeMode365 <codemode365@gmail.com>
2023-06-02 02:58:49 +0100

    tabulated

--- dist/dynamic/countConstruct.js
+++ dist/dynamic/countConstruct.js
@@ -41,3 +41,28 @@ console.log(memoCountConstruct("eeeeeeeeeeeeeeeeeeeeeee", [
     "eeee",
 ]));
 console.log(countConstruct("eeeeeeeeeeeeeeeeeeeeeee", ["e", "ee", "eee", "eeee", "eeee"]));
+/**
+ *
+ * Tabulation Solutio
+ *
+ * m=target length
+ * n=words length
+ *
+ * Compleixty
+ *  Time: m*n*m //extract m is due to slicing
+ *  Space: m
+ */
+function tabCountConstruct(target, wordBank) {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+    for (let x = 0; x <= target.length; x++) {
+        for (let word of wordBank) {
+            if (target.slice(x, x + word.length) == word) {
+                table[x + word.length] += table[x];
+            }
+        }
+    }
+    return table[target.length];
+}
+console.log(tabCountConstruct("purple", ["pur", "p", "ur", "le", "purpl"])); //2
+console.log(tabCountConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); //`
--- src/dynamic/countConstruct.ts
+++ src/dynamic/countConstruct.ts
@@ -75,3 +75,32 @@ console.log(
 console.log(
   countConstruct("eeeeeeeeeeeeeeeeeeeeeee", ["e", "ee", "eee", "eeee", "eeee"])
 );
+
+/**
+ *
+ * Tabulation Solutio
+ *
+ * m=target length
+ * n=words length
+ *
+ * Compleixty
+ *  Time: m*n*m //extract m is due to slicing
+ *  Space: m
+ */
+
+function tabCountConstruct(target: string, wordBank: string[]): number {
+  const table = Array(target.length + 1).fill(0);
+  table[0] = 1;
+
+  for (let x = 0; x <= target.length; x++) {
+      for (let word of wordBank) {
+          if (target.slice(x, x + word.length) == word) {
+            table[x + word.length] += table[x];
+        }
+    }
+  }
+  return table[target.length];
+}
+
+console.log(tabCountConstruct("purple", ["pur", "p", "ur", "le", "purpl"])); //2
+console.log(tabCountConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); //`
