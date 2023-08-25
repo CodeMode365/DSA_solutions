ad449bf15d111f1b09263ba75914c0e72b3ea152
Pabin234 <102400760+CodeMode365@users.noreply.github.com>
2023-08-25 19:43:02 +0530

    Deleted dist

--- dist/recursion/decimalToBinary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const decimalToBinary = (input) => {
-    if (input == 0)
-        return input.toString();
-    if (input == 1)
-        return input.toString();
-    return `${decimalToBinary(Math.floor(input / 2))}${input % 2}`;
-};
-console.log(decimalToBinary(233));
